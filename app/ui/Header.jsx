458a4c86693fbfafea6f865e092b381945b46baf
sandy424 <sandy424@naver.com>
2026-04-01 20:05:36 +0900

    tailwindcss 설치, header layout 적용

---
+++ app/ui/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header() {
+    return(
+        <div className="flex items-center justify-center p-4">
+            <h3 className="font-en font-semibold text-green-600 text-2xl">EZREAD</h3>
+        </div>
+    )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./app/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        en: ['Georgia', 'Times New Roman', 'serif'],
+      },
+    },
+  },
+  plugins: [],
+}
+
