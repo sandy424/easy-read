aea8b544cd05d59f0eb905ed4ba01a61665f0d45
sandy424 <sandy424@naver.com>
2026-04-07 16:51:43 +0900

    result page 초기 화면

---
+++ app/result/page.tsx
@@ -0,0 +1,10 @@
+export default function Result() {
+    return(
+        <div className="bg-green-100 w-full flex flex-col items-center p-28 font-ko">
+            <h1 className="font-bold text-xl">결과를 보여드릴게요</h1>
+            <div className="border border-green-300 rounded-xl p-4 mt-8 w-[800px] h-[600px] bg-white/70 flex flex-col items-center justify-center">
+                
+            </div>
+        </div>
+    )
+}
